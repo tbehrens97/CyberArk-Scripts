24fd736b9a9f433c785364fe3635201ee75f2472
Trevor Behrens <laxdale205@aol.com>
2025-04-25 15:25:07 -0400

    Removed WinRM items from the local only scripts

--- CyberArk PowerShell Scripts/Update SSL Certificates/IIS SSL Certificate/Local SSL Update/Script to deploy Certs.ps1
+++ CyberArk PowerShell Scripts/Update SSL Certificates/IIS SSL Certificate/Local SSL Update/Script to deploy Certs.ps1
@@ -14,14 +14,10 @@ function Get-FileName($initialDirectory)
 
 $FileName=Get-FileName
 
-#get just file name with extension
-$outputPath = $FileName
-$outputFile = Split-Path $outputPath -leaf
-
 #Get Cert password
 $cred=Read-Host "Enter The Certificate Password" -AsSecureString
 
-$CertData=Get-PfxData -FilePath $outputPath -Password $cred
+$CertData=Get-PfxData -FilePath $FileName -Password $cred
 
 $CertThumb=$CertData.EndEntityCertificates | select Thumbprint
 #Cert thumprint
@@ -30,12 +26,12 @@ $CertThumbPrint=$CertThumb.Thumbprint
 
 Set-Location -Path cert:\localMachine\my
 
-Import-PfxCertificate -FilePath "C:\$outputFile" -Password $cred
+Import-PfxCertificate -FilePath $FileName -Password $cred
 
 Import-Module WebAdministration
 
 Remove-Item -path 'IIS:\SslBindings\0.0.0.0!443' -Force
 
-Get-Item "Cert:\LocalMachine\My\$Using:CertThumbPrint" | New-Item -path 'IIS:\SslBindings\0.0.0.0!443' -Force
+Get-Item "Cert:\LocalMachine\My\$CertThumbPrint" | New-Item -path 'IIS:\SslBindings\0.0.0.0!443' -Force
 
 iisreset
--- CyberArk PowerShell Scripts/Update SSL Certificates/RDP SSL Certificate/Local SSL Update/Script to deploy Certs Local (PSM RDP SSL).ps1
+++ CyberArk PowerShell Scripts/Update SSL Certificates/RDP SSL Certificate/Local SSL Update/Script to deploy Certs Local (PSM RDP SSL).ps1
@@ -13,9 +13,6 @@
 
 $FileName=Get-FileName
 
-#get just file name with extension
-$outputPath = $FileName
-$outputFile = Split-Path $outputPath -leaf
 
 #Get Cert password
 $cred=Read-Host "Enter The Certificate Password" -AsSecureString
@@ -29,10 +26,10 @@ $CertThumbPrint=$CertThumb.Thumbprint
 
 Set-Location -Path cert:\localMachine\my
 
-Import-PfxCertificate -FilePath "C:\$outputFile" -Password $cred
+Import-PfxCertificate -FilePath $FileName -Password $cred
 
 $tsgs = gwmi -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -Filter "TerminalName='RDP-tcp'"
 
-swmi -path $tsgs.__path -argument @{SSLCertificateSHA1Hash="$($Using:CertThumbPrint)"}
+swmi -path $tsgs.__path -argument @{SSLCertificateSHA1Hash="$($CertThumbPrint)"}
 
 
